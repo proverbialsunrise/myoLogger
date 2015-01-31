27645724f057e5c4676c10fd4a025d875e7a85c2
Daniel Johnson <dan@ddajohnson.com>
2015-01-31 14:58:08 -0500

    Initial Commit

---
+++ MyoLogger/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyoLogger
+//
+//  Created by Daniel Johnson on 1/31/15.
+//  Copyright (c) 2015 Daniel Johnson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
